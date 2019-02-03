e0a3f3ff1285dbf51e024d71de3efe96ef7ac758
Jeffrey T. Fritz <csharpfritz@users.noreply.github.com>
2019-02-03 12:18:28 -0500

    Started work on managing a Wemo

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using McMaster.Extensions.CommandLineUtils;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Settings.Configuration;
+using StreamDeckLib;
+using System;
+using System.IO;
+using System.Reflection;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Configuration;
+using System.Diagnostics;
+
+namespace Fritz.SmartHome
+{
+	    class Program
+    {
+        static ILoggerFactory GetLoggerFactory()
+        {
+            var dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            Directory.SetCurrentDirectory(dir);
+
+            var configuration = new ConfigurationBuilder()
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+            Log.Logger = new LoggerConfiguration()
+                    .ReadFrom.Configuration(configuration)
+                    .CreateLogger();
+
+            var loggerFactory = new LoggerFactory()
+                .AddSerilog(Log.Logger);
+
+            TopLogger = loggerFactory.CreateLogger("top");
+
+            TopLogger.LogInformation("Plugin started");
+
+            return loggerFactory;
+        }
+
+        private static Microsoft.Extensions.Logging.ILogger TopLogger;
+
+        static async Task Main(string[] args)
+        {
+
+
+            using (var source = new CancellationTokenSource())
+            {
+                using (var loggerFactory = GetLoggerFactory())
+                {
+                    try
+                    {
+
+                        await ConnectionManager.Initialize(args, loggerFactory)
+                            .SetPlugin(new WemoPlugin() { Logger = loggerFactory.CreateLogger("WeMo") })
+                            .StartAsync(source.Token);
+
+                        //Console.ReadLine();
+
+                    }
+                    catch (Exception ex)
+                    {
+                        TopLogger.LogError(ex, "Error while running the plugin");
+                    }
+                    source.Cancel();
+                }
+
+            }
+        }
+    }
+}
---
+++ WemoPlugin.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Extensions.Logging;
+using StreamDeckLib;
+using StreamDeckLib.Messages;
+using System.Dynamic;
+using System.Threading.Tasks;
+using WemoNet;
+
+namespace Fritz.SmartHome
+{
+	internal class WemoPlugin : BaseStreamDeckPlugin
+	{
+
+		// Cheer 242 cpayette 03/2/19 
+
+		private readonly Wemo _MyWemo = new Wemo();
+		private bool _State = false;
+		private string _IpAddress = "";
+
+		public ILogger Logger { get; set; }
+
+		public override async Task OnKeyUp(StreamDeckEventPayload args)
+		{
+
+			// _MyWemo.
+
+			if (_State) {
+				Logger.LogDebug($"Turning off device at {_IpAddress}");
+				await _MyWemo.TurnOffWemoPlugAsync(_IpAddress);
+				// await this.Manager.SetImageAsync(args.context, "??");
+			} else {
+				Logger.LogDebug($"Turning on device at {_IpAddress}");
+				await _MyWemo.TurnOnWemoPlugAsync(_IpAddress);
+				// await this.Manager.SetImageAsync(args.context, "??");
+			}
+
+		}
+
+		public override async Task OnWillAppear(StreamDeckEventPayload args)
+		{
+			this._IpAddress = args.payload.settings?.IpAddress ?? "";
+			this._State = args.payload.settings?.State ?? false;
+		}
+
+		public override async Task OnWillDisappear(StreamDeckEventPayload args)
+		{
+
+			var settings = new { 
+				IpAddress = _IpAddress,
+				State = _State
+			};
+
+			await Manager.SetSettingsAsync(args.context, settings);
+		}
+	}
+
+
+}
